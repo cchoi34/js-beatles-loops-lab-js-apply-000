4f9507fbadd044fe9c98be3c6dcd5a3f842d3829
generic-tag-3052 <christopherchoi97@gmail.com>
2019-06-12 16:03:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
+  var newArray = [];
+  for (var i = 0; i < arrayOfMusicians.length; i++) {
+    for (var j = 0; i < arrayOfInstruments.length; i++) {
+      var playerInstrument = `${arrayOfMusicians[i]} plays ${arrayOfInstruments[j]}`;
+      newArray.push(playerInstrument);
+    }
+  }
+  return newArray;
+}
+
+function johnLennonFacts(johnFacts) {
+  let counter = 0;
+  let excitedJohnFacts = [];
+  while (counter < johnFacts.length) {
+    excitedJohnFacts.push(johnFacts[counter] + '!!!');
+    counter++;
+  }
+  return excitedJohnFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var lovingTheBeatles = [];
+  let counter = 0;
+  
+  do {
+    lovingTheBeatles
+  }
+}
